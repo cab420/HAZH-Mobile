fc5b53633ec8ff532a96213262602b6aa0bbe169
cab420 <85059403+cab420@users.noreply.github.com>
2023-03-11 17:40:43 +1000

    packages installation

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
+import "react-native-gesture-handler";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+
+//this stack holds all the screens you swipe through on the app
+//const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>test</Text>
+          <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3BC6D7',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+//stuff to install
+//npm install @react-navigation/native-stack
+//npx expo install react-native-screens react-native-safe-area-context
+//npm install @react-navigation/native   
+//npx expo install react-native-gesture-handler
+//npm install react-native-reanimated
