87516f2bef0c1b364b2602c5dbe354876ba08983
HannahSchluter <hannah.schluter@connect.qut.edu.au>
2023-04-03 18:07:20 +1000

    Added background image to home and history

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,46 +4,55 @@ import { Button, Input, Image } from "react-native-elements";
 import { AuthContext } from '../context/AuthContext';
 import { startRecording } from '../components/ScreenShare';
 
+import {ImageBackground} from 'react-native'; // for background image
+
 const HomeScreen = ({ navigation }) => {
     const {isLoading, logout, userInfo} = useContext(AuthContext);
     
+    const localImage = require('../assets/greyscaleQPSlogo.png'); // for background image
+    
     return (
       <KeyboardAvoidingView behavior="padding" style={styles.container}>
-        <View>
-          <Text>Welcome {userInfo.name}</Text>
+        <ImageBackground source={localImage} style={styles.image}>
+
+          <Text style={styles.welcomeText}>Welcome {userInfo.name}</Text>
           <Text></Text>
           <Text></Text>
+        
           <Button 
             onPress={() => { 
               startRecording();             
             }}
-            containerStyle={styles.Button} title="Share to Desktop" 
+            containerStyle={styles.button} title="Share to Desktop" 
           />
+        
           <Text></Text>
           <Button 
             onPress={() => {
               navigation.navigate("RecordHistory")
             }}
-            containerStyle={styles.Button} title="Screen Records History" 
+            containerStyle={styles.button} title= "Screen Records History" 
           />
+        
           <Text></Text>
           <Button 
             onPress={() => {
               //logout();
               navigation.navigate("Login")
             }}
-            containerStyle={styles.Button} title="Logout" 
+            containerStyle={styles.button} title="Logout" 
           />
-          
-          
-        </View>
+
+          </ImageBackground>
+
+        
       </KeyboardAvoidingView>
     )
   }
 
 export default HomeScreen
 
-//style sheet for different things on login screen
+//style sheet for different things on home screen
 const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -55,11 +64,23 @@ const styles = StyleSheet.create({
       width: 300,
     },
     button: {
-      width: 200,
-      marginBottom: 200,
+      //width: 200,
+      marginBottom: 20,
     },
-    logo: {
-      width:200,
-      height: 250
+    
+    welcomeText: { //format the text at top of page to welcome user
+      fontSize: 20,
+      fontWeight: "bold",
+      textAlign: 'center',
+      //height: 100,
     },
+    
+    image: { // the background image
+      //flex: 1,
+      justifyContent: 'center',
+      resizeMode: 'contain',
+      height: 300,
+      width: 250,
+    }
+
   });
--- screens/RecordHistory.js
+++ screens/RecordHistory.js
@@ -1,24 +1,34 @@
 import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput } from 'react-native'
 import React, {useState} from 'react'
-import { Button, Input, Image } from "react-native-elements";       
+import { Button, Input, Image } from "react-native-elements";     
+import {ImageBackground} from 'react-native'; // for background image
+
 
 const RecordHistoryScreen = ({ navigation }) => {
+  
+  const localImage = require('../assets/greyscaleQPSlogo.png'); // for background image
+
   return (
     <KeyboardAvoidingView behavior="padding" style={styles.container}>
-    <View style={styles.inputContainer}>
+      <ImageBackground source={localImage} style={styles.image}>
 
-      <Text style = {styles.msg}>Screen Record History</Text>
-      
-      <Button onPress={() => navigation.navigate("Home")} containerStyle={styles.button} title="Return Home" />
 
-    </View>
+        <Text style = {styles.msg}>Screen Record History</Text>
+        <Text></Text>
+        <Text></Text>
+
+        <Button onPress={() => navigation.navigate("Home")} 
+          containerStyle={styles.button} title="Return Home" />
+        
+
+      </ImageBackground>
     </KeyboardAvoidingView>
   )
 }
 
 export default RecordHistoryScreen
 
-//style sheet for different things on login screen
+//style sheet for different things on record history screen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -28,25 +38,32 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     width: 300,
+    //flex: null,
   },
   button: {
-    width: 300,
-    marginTop: 30,
+    //width: 300,
+    marginBottom: 20,
   },
-  logo: {
-    width:200,
-    height: 250
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
+  //input: {
+    //height: 40,
+    //margin: 12,
+    //borderWidth: 1,
+    //padding: 10,
+  //},
   msg: {
     textAlign: 'center',
     fontSize: 20,
     fontWeight: 'bold',
-    height: 100,
+    //height: 100,
   },
+
+  image: { // the background image
+    //flex: 1,
+    justifyContent: 'center',
+    resizeMode: 'contain',
+    height: 300,
+    height: 300,
+    width: 250,
+  }
+
 });
